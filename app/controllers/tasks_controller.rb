67fcc1c0f999ff515f4b9590261eb7a3e7d1d526
Pol Suar <pablors88@gmail.com>
2021-05-09 23:43:36 -0300

    Add style and structure to app

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
 
   def index
     if params[:category].blank?
-      @task = Task.all.order("created_at DESC")
+      @tasks = Task.all.order("created_at DESC")
     else
       @category_id = Category.find_by(name: params[:category]).id
       @tasks = Task.where(category_id: @category_id).order("created_at DESC")
